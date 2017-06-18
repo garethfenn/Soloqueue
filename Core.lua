febfe05de78be7e1bfc3e63ab1add8a9353d665b
Gareth Fenn <garethfenn87@gmail.com>
2017-06-18 20:45:23 +0100

    Cleaning up

--- Core.lua
+++ Core.lua
@@ -1,22 +1,52 @@
 Soloqueue = LibStub("AceAddon-3.0"):NewAddon("Soloqueue", "AceConsole-3.0");
-
 local eventFrame = nil
-local BRACKETS = { "2v2", "3v3", "5v5", "RBG" }
 
+-- Operation types
+local OP_INVALID, OP_QUEUE, OP_CHECKTEAMMATES = 0, 1, 2;
+local operation = OP_INVALID;
+
+-- Stack of players to get ratings from
 local player_stack = {}
 local player_stack_idx = 0
 
+-- Sometimes the ratings are not available first time around
 local attempts = 0
 local MAX_ATTEMPTS = 3
+
+-- 5v5 isn't used anymore...
+local BRACKETS = { "2v2", "3v3", "5v5", "RBG" }
+
+-- Forward declarate some functions
+local getCurrentRatings
 
 local SoloqueueLDB = LibStub("LibDataBroker-1.1"):NewDataObject("Soloqueue", {
 	type = "data source",
 	text = "Soloqueue",
-	icon = "Interface\\Icons\\INV_Chest_Cloth_17",
-	OnClick = function() print("BUNNIES ARE TAKING OVER THE WORLD") end,
+	icon = "Interface\\Icons\\Achievement_arena_2v2_7",
+	OnClick = function(clickedframe, button) Soloqueue:FindGroup() end,
 });
 
 local icon = LibStub("LibDBIcon-1.0");
+
+function Soloqueue:CurPlayer()
+	if player_stack_idx > 0 then
+		return player_stack[player_stack_idx];
+	else
+		return nil
+	end
+end
+
+function Soloqueue:PutPlayer(player)
+	--print ("Put " .. player)
+	player_stack_idx = player_stack_idx + 1
+	player_stack[player_stack_idx] = player;
+end
+
+function Soloqueue:PopPlayer()
+	local player = self:CurPlayer();
+	--print ("Pop " .. player);
+	player_stack_idx = player_stack_idx - 1;
+end
 
 local function PrintRatings(player, ratings)
 	local name = UnitName(player);
@@ -26,7 +56,40 @@ local function PrintRatings(player, ratings)
   	end
 end
 
-local function getCurrentRatings()
+local function eventHandler(self, event)
+	--print("got event:" .. event)
+  	if event == "INSPECT_HONOR_UPDATE" then
+  		eventFrame:UnregisterEvent("INSPECT_HONOR_UPDATE");
+  		getCurrentRatings();
+  	elseif event == "INSPECT_READY" then
+  		eventFrame:UnregisterEvent("INSPECT_READY");
+  	    eventFrame:RegisterEvent("INSPECT_HONOR_UPDATE");
+ 		RequestInspectHonorData();
+  	end
+end
+
+function Soloqueue:OnInitialize()
+	self:Print("Soloqueue")
+	self.db = LibStub("AceDB-3.0"):New("SoloqueueDB", {
+		profile = {
+			minimap = {
+				hide = false,
+			},
+		},
+	});
+
+	-- Commands
+	self:RegisterChatCommand("soloqueue", "CheckTeammates");
+
+	-- Minimap
+	icon:Register("Soloqueue", SoloqueueLDB, self.db.profile.minimap);
+
+	-- Set up event handler
+	eventFrame = CreateFrame("Frame", "SoloqueueEventFrame", UIParent)
+	eventFrame:SetScript("OnEvent", eventHandler);
+end;
+
+function getCurrentRatings()
 	local succsess = false;
 	local player = Soloqueue:CurPlayer();
 
@@ -57,78 +120,6 @@ local function getCurrentRatings()
 	end
 end
 
-local function eventHandler(self, event)
-	--print("got event:" .. event)
-  	if event == "INSPECT_HONOR_UPDATE" then
-  		eventFrame:UnregisterEvent("INSPECT_HONOR_UPDATE");
-  		getCurrentRatings();
-  	elseif event == "INSPECT_READY" then
-  		eventFrame:UnregisterEvent("INSPECT_READY");
-  	    eventFrame:RegisterEvent("INSPECT_HONOR_UPDATE");
- 		RequestInspectHonorData();
-  	end
-end
-
-function Soloqueue:OnInitialize()
-	self:Print("Soloqueue")
-	self.db = LibStub("AceDB-3.0"):New("SoloqueueDB", {
-		profile = {
-			minimap = {
-				hide = false,
-			},
-		},
-	});
-
-	-- Commands
-	self:RegisterChatCommand("soloqueue", "GetAllRatings");
-
-	-- Minimap
-	icon:Register("Soloqueue", SoloqueueLDB, self.db.profile.minimap);
-
-	-- Set up event handler
-	eventFrame = CreateFrame("Frame", "SoloqueueEventFrame", UIParent)
-	eventFrame:SetScript("OnEvent", eventHandler);
-end;
-
-function Soloqueue:CurPlayer()
-	if player_stack_idx > 0 then
-		return player_stack[player_stack_idx];
-	else
-		return nil
-	end
-end
-
-function Soloqueue:PutPlayer(player)
-	--print ("Put " .. player)
-	player_stack_idx = player_stack_idx + 1
-	player_stack[player_stack_idx] = player;
-end
-
-function Soloqueue:PopPlayer()
-	local player = self:CurPlayer();
-	--print ("Pop " .. player);
-	player_stack_idx = player_stack_idx - 1;
-end
-
-function Soloqueue:GetAllRatings()
-	self:Print("Getting ratings...");
-
-	local selfName = UnitName('player');
-
-	self:PutPlayer("player");
-
-	if IsInRaid() then
-	    for i = 1, 10 do
-	    	local playerName = UnitName('raid' .. i);
-	        if (playerName and (playerName ~= selfName)) then
-	            self:PutPlayer('raid' .. i);
-	        end
-	    end
-	end
-
-	self:GetRatings();
-end
-
 function Soloqueue:GetRatings()
 	local player = Soloqueue:CurPlayer();
 	if (player) then
@@ -136,3 +127,25 @@ function Soloqueue:GetRatings()
     	NotifyInspect(player);
     end
 end;
+
+function Soloqueue:FindGroup()
+	operation = OP_QUEUE;
+	self:Print("Finding group...");
+	self:PutPlayer("player");
+	self:GetRatings();
+end
+
+function Soloqueue:CheckTeammates()
+	self:Print("Checking teammates ratings are correct...");
+
+	local selfName = UnitName('player');
+	if IsInRaid() then
+	    for i = 1, 10 do
+	    	local playerName = UnitName('raid' .. i);
+	        if (playerName and (playerName ~= selfName)) then
+	            self:PutPlayer('raid' .. i);
+	        end
+	    end
+	    self:GetRatings();
+	end
+end
